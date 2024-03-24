033637a6f329a71ebd2c551ab62023f8d8bbf8f5
Shiv-Bhushan-Singh <djudge7500@gmail.com>
2024-03-24 12:52:27 +0530

    connecting

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// require('dotenv').config({path: './env'})  This is one of the way to use it.
+
+import dotenv from 'dotenv';  
+import connectDB from './db/index.js'; 
+
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+/*
+import express from 'express';
+const app = express()
+
+----- Method 1 for connecting to the Database. Good Approach.-----
+; (async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error:", (error) => {
+            console.log("ERR: ", error)
+        })
+
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${ process.env.PORT }`)
+        })
+    } catch (error) {
+        console.error("ERROR :", error)
+        throw error
+    }
+})()
+*/
